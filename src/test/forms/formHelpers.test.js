47948537aa8cd83805b0973a6e68e80623a23732
Ethan Blackwood <ethanbblackwood@gmail.com>
2018-03-27 19:37:10 -0400

    Add a few placeholders

--- src/test/forms/formHelpers.test.js
+++ src/test/forms/formHelpers.test.js
@@ -2,3 +2,19 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import * as formHelpers from '../../forms/formHelpers';
+
+describe('ManagedNumberField', () => {
+  
+});
+
+describe('CashFlowRow', () => {
+
+});
+
+describe('MonthlyCashFlowRow', () => {
+
+});
+
+describe('CashFlowContainer', () => {
+
+});
