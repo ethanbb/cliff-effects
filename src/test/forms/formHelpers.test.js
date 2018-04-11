933213c7a2dd336beb6c609e0a463716883e49a1
Ethan Blackwood <ethanbblackwood@gmail.com>
2018-04-10 21:36:10 -0400

    Write test which is now broken

--- src/test/forms/formHelpers.test.js
+++ src/test/forms/formHelpers.test.js
@@ -4,7 +4,27 @@ import { shallow } from 'enzyme';
 import * as formHelpers from '../../forms/formHelpers';
 
 describe('ManagedNumberField', () => {
-  
+  const equalsOne = (x) => x == 1;
+
+  test('Shows error when value changes to something invalid', () => {
+    const wrapper = shallow((
+      <formHelpers.ManagedNumberField
+        store={() => {}}
+        name={'test'}
+        validation={equalsOne}
+        value={'1'}
+        className={''}
+        format={(x) => x.toString()}
+      />
+    ));
+
+    const input = wrapper.at(0);
+    expect(input.prop('error')).toBe(false);
+    input.simulate('focus');
+    input.simulate('keyDown', { key: '2' });
+    console.log(input.prop('value'));
+    expect(input.prop('error')).toBe(true);
+  })
 });
 
 describe('CashFlowRow', () => {
